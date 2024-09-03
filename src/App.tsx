20c126a92395ef51e95cd05c1f9eab220baecc38
Sara-Jo <sarajo.dev@gmail.com>
2024-09-03 18:15:07 +0900

    feat: apply inifiteQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { fetchPokemon } from "./api";
+import "./App.css";
+import React, { useCallback, useEffect, useRef } from "react";
+import { PokemonResult } from "./types";
 
 function App() {
+  const {
+    data,
+    isLoading,
+    isError,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
+    queryKey: ["pokemon"],
+    queryFn: fetchPokemon,
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => {
+      return lastPage.isLast ? undefined : lastPage.nextPage;
+    },
+  });
+
+  const observerElem = useRef<HTMLDivElement | null>(null);
+
+  const observerCallback = useCallback(
+    (entries: IntersectionObserverEntry[]) => {
+      const [entry] = entries;
+      if (entry.isIntersecting && hasNextPage) {
+        fetchNextPage();
+      }
+    },
+    [fetchNextPage, hasNextPage]
+  );
+
+  // Intersection Observer 설정 및 해제
+  useEffect(() => {
+    const currentElem = observerElem.current;
+    const observer = new IntersectionObserver(observerCallback, {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1,
+    });
+
+    if (currentElem) {
+      observer.observe(currentElem);
+    }
+
+    return () => {
+      if (currentElem) {
+        observer.unobserve(currentElem);
+      }
+    };
+  }, [observerCallback]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1 className="title">Pokemon</h1>
+      <div className="pokemon-list">
+        {data?.pages.map((page, pageIndex) => (
+          <React.Fragment key={pageIndex}>
+            {page.results.map((pokemon: PokemonResult, index: number) => (
+              <div key={index} className="pokemon-card">
+                <h3>{pokemon.name}</h3>
+                <img src={pokemon.image} alt={pokemon.name} />
+                <p>{pokemon.type}</p>
+              </div>
+            ))}
+          </React.Fragment>
+        ))}
+        {isFetchingNextPage && <p>Loading more...</p>}
+      </div>
+      <div ref={observerElem}></div>
     </div>
   );
 }
